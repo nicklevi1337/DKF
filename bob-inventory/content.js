dbdc4c397f77c0b8554317eaa0711347270bc3fe
PrettyLolli <yunghunters2015@gmail.com>
2023-08-25 00:45:26 +0300

    осталось доделать добавление строк

--- bob-inventory/content.js
+++ bob-inventory/content.js
@@ -23,6 +23,26 @@ const formPointsSave = document.querySelector(".popup__form_type_points");
 
 
 
+// Попап для таблицы
+const openAddList =  document.querySelector('.add-text');
+const popupAddDay =  document.querySelector('.popup__input_type_day');
+const popupAddPlay =  document.querySelector('.popup__input_type_play');
+const popupAddLink =  document.querySelector('.popup__link');
+const popupImgLink =  document.querySelector('.popup__link');
+
+const popupAddComm =  document.querySelector('.popup__input_type_comm');
+const formElementAdd = document.querySelector(".popup__container_type_stats");
+const formSaveAdd = document.querySelector(".popup__form_type_stats");
+
+const popupAddList =  document.querySelector('.popup_type_stats');
+const popupCloseBtnAdd = document.querySelector(".popup__close_type_stats");
+
+const popupSaveBtnAdd = document.querySelector(".popup__save-btn");
+const listTemplate = document.getElementById("list-template");
+const listContainer = document.querySelector(".list");
+
+
+
 
 
 
@@ -33,6 +53,50 @@ function openPopup(popup) {
 function closePopup(popup) {
     popup.classList.remove('popup_opened')
 }
+
+
+
+const popupAdd  = () => {
+    openPopup(popupAddList)
+    
+}
+
+const popupAddClosr  = () => {
+    closePopup(popupAddList)
+    
+}
+
+function closePopupAddByClick(evt) {
+    const isOverlay = evt.target.classList.contains("popup_type_stats");
+    const isCloseBtn = evt.target.classList.contains("popup__close");
+    if (isOverlay || isCloseBtn) {
+        closePopup(popupAddList);
+}
+}
+
+const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+
+    const name = popupAddDay.value;
+    const link = popupAddLink.value;
+    const title = popupAddPlay.value;
+    const img = popupImgLink.value;
+    const comment = popupAddComm.value;
+    
+
+    const infoList = {
+        name,
+        link,
+        title,
+        img,
+        comment
+    };
+
+    renderAddElement(createListElement(infoList)); 
+    closePopup(popupAddList);
+    event.target.reset(event);
+};
+
 
 
 const popupGameOpen = () => {
@@ -93,6 +157,60 @@ function loadNumberFromLocalStorage(key) {
 }
 
 
+
+
+
+
+
+const infoList = [
+    {
+        name: "1",
+        link: "https://ic.wampi.ru/2023/08/25/START.png",
+        title: "Старт",
+        img: "https://ic.wampi.ru/2023/08/24/PROIDENO.png",
+        comment: "Let's Go!"
+    },
+
+];
+
+const createListElement = (listData) => {
+    const listElement = listTemplate.content.querySelector(".list__check").cloneNode(true);
+    const listName = listElement.querySelector(".number__subtitle");
+    const listLink = listElement.querySelector(".mission__icon");
+    const listTitle = listElement.querySelector(".game-text__icon");
+    const listImg = listElement.querySelector(".status__text");
+    const listComm = listElement.querySelector(".comment__text");
+
+
+    listName.textContent = listData.name;
+    listLink.src = listData.link;
+    listLink.alt = listData.name;
+    listTitle.textContent = listData.title;
+    listImg.src = listData.img;
+    listImg.alt = listData.name;
+    listComm.textContent = listData.comment;
+    
+
+   
+    
+
+    
+    
+    return listElement;
+};
+
+const renderAddElement = (listElement) => {
+    listContainer.append(listElement);
+};
+
+infoList.forEach((list) => {
+    const element = createListElement(list);
+    renderAddElement(element);
+});
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const savedPointsBobValue = loadNumberFromLocalStorage('pointsBobValue');
     if (savedPointsBobValue !== null) {
@@ -104,6 +222,8 @@ document.addEventListener('DOMContentLoaded', () => {
         gameTextBob.textContent = savedGameBobValue;
     }
 });
+
+
 
 
 openPointsBobBtn.addEventListener("click", pointsBobEdit);
@@ -121,3 +241,10 @@ popupCloseBtn.addEventListener("click", () => closePopup(popupGame));
 
 
 
+
+
+openAddList.addEventListener("click", () => openPopup(popupAddList));
+popupCloseBtnAdd.addEventListener("click", () => closePopup(popupAddList));
+popupAddList.addEventListener("click", closePopupAddByClick);
+formElementAdd.addEventListener("submit", handleAddFormSubmit); 
+formSaveAdd.addEventListener("click", closePopupAddByClick); 
